9998b53be4ee0489c365bffb3939117400e72407
Adavak LEE <adavak@adavak.com>
2020-02-08 16:15:03 +0800

    Update zhCN.lua

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -4,17 +4,17 @@ if not L then return end
 
 -- Option menu strings.
 L["Rare window scale"] = "稀有窗口缩放"
--- L["Set the scale of the rare window."] = ""
+L["Set the scale of the rare window."] = "设定稀有窗口缩放。"
 L["Disable All"] = "禁用全部"
--- L["Disable all non-favorite rares in the list."] = ""
+L["Disable all non-favorite rares in the list."] = "禁用全部非偏好稀有列表。"
 L["Enable All"] = "启用全部"
--- L["Enable all rares in the list."] = ""
+L["Enable all rares in the list."] = "启用全部稀有列表。"
 L["Reset Favorites"] = "重置偏好"
-L["Reset the list of favorite rares."] = ""
--- L["General Options"] = ""
--- L["Rare List Options"] = ""
--- L["Active Rares"] = ""
+L["Reset the list of favorite rares."] = "重置偏好稀有列表。"
+L["General Options"] = "通用选项"
+L["Rare List Options"] = "偏好列表选项"
+L["Active Rares"] = "激活稀有"
 
 -- Status messages.
 L["<%s> Moving to shard "] = "<%s> 移动到分片 "
-L["<%s> Failed to register AddonPrefix '%s'. %s will not function properly."] = "<%s> 无法注册插件前缀 '%s'. %s无法正常运行."
+L["<%s> Failed to register AddonPrefix '%s'. %s will not function properly."] = "<%s> 无法注册插件前缀 '%s'。%s无法正常运行。"
